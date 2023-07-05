8666b70be029a76abb01b4e4bbef8fa79717464c
Drakkarrr <junrey404@gmail.com>
2023-07-06 06:42:54 +0800

    featured component added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './home.scss';
+import Featured from '../../components/featured/Featured';
 
 const Home = () => {
-  return <div className='home'>Home</div>;
+  return (
+    <div className='home'>
+      <Featured />
+    </div>
+  );
 };
 
 export default Home;
