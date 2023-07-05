b2ecb8e34c3e8fb290d5945b7110220c44327230
Drakkarrr <junrey404@gmail.com>
2023-07-06 06:24:20 +0800

    setup routing based and layout outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,80 @@
 import React from 'react';
 import './App.css';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Gigs from './pages/gigs/Gigs';
+import Gig from './pages/gig/Gig';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Add from './pages/addGig/Add';
+import Orders from './pages/orders/Orders';
+import Messages from './pages/messages/Messages';
+import Message from './pages/message/Message';
+import MyGigs from './pages/myGigs/MyGigs';
 
-function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
-}
+const App = () => {
+  const Layout = () => {
+    return (
+      <div className='app'>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/gigs',
+          element: <Gigs />,
+        },
+        {
+          path: '/myGigs',
+          element: <MyGigs />,
+        },
+        {
+          path: '/orders',
+          element: <Orders />,
+        },
+        {
+          path: '/messages',
+          element: <Messages />,
+        },
+        {
+          path: '/message/:id',
+          element: <Message />,
+        },
+        {
+          path: '/add',
+          element: <Add />,
+        },
+        {
+          path: '/gig/:id',
+          element: <Gig />,
+        },
+      ],
+    },
+    {
+      path: '/register',
+      element: <Register />,
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
 
 export default App;
