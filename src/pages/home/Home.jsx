8409067e033f010a7d5b72b31808bcef39081bd9
Drakkarrr <junrey404@gmail.com>
2023-07-06 18:01:20 +0800

    implemented add trusted by companies component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './home.scss';
 import Featured from '../../components/featured/Featured';
+import TrustedBy from '../../components/trustedBy/TrustedBy';
 
 const Home = () => {
   return (
     <div className='home'>
       <Featured />
+      <TrustedBy />
     </div>
   );
 };
