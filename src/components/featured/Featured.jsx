050c7ee7ee82a83dbb066681b60c8ea096defdf7
Drakkarrr <junrey404@gmail.com>
2023-07-06 16:57:28 +0800

    featured pages inserted

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './featured.scss';
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+      <div className='container'>
+        <h1>hello featured cards</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
