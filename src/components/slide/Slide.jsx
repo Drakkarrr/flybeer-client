1d24901b2723e90ae3b39c243cf203c05e174754
Drakkarrr <junrey404@gmail.com>
2023-07-09 09:16:37 +0800

    added infinite slide carousel

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './slide.scss';
+import Slider from 'infinite-react-carousel';
+import CategoryCard from '../categoriesGigs/CategoryCard';
+import { cards } from '../../data';
 
 const Slide = () => {
   return (
     <div className='slide'>
       <div className='container'>
-        <h1>test slides</h1>
+        <Slider slidesToShow={5}>
+          {cards.map((card) => (
+            <CategoryCard item={card} key={card.id} />
+          ))}
+        </Slider>
       </div>
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './home.scss';
 import Featured from '../../components/featured/Featured';
 import TrustedBy from '../../components/trustedBy/TrustedBy';
+import Slide from '../../components/slide/Slide';
 
 const Home = () => {
   return (
     <div className='home'>
       <Featured />
       <TrustedBy />
+      <Slide />
     </div>
   );
 };
