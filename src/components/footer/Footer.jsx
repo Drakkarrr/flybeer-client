103ce2e330ed028f6d0f5bb12669b4f18fe95093
Drakkarrr <junrey404@gmail.com>
2023-07-13 08:41:09 +0800

    added footer component with navlinks

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,98 @@
 import React from 'react';
-import './footer.scss';
+import './Footer.scss';
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className='container'>
+        <div className='top'>
+          <div className='item'>
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className='item'>
+            <h2>About</h2>
+            <span>Press & News</span>
+            <span>Partnerships</span>
+            <span>Privacy Policy</span>
+            <span>Terms of Service</span>
+            <span>Intellectual Property Claims</span>
+            <span>Investor Relations</span>
+            <span>Contact Sales</span>
+          </div>
+          <div className='item'>
+            <h2>Support</h2>
+            <span>Help & Support</span>
+            <span>Trust & Safety</span>
+            <span>Selling on Flybeer</span>
+            <span>Buying on Flybeer</span>
+          </div>
+          <div className='item'>
+            <h2>Community</h2>
+            <span>Customer Success Stories</span>
+            <span>Community hub</span>
+            <span>Forum</span>
+            <span>Events</span>
+            <span>Blog</span>
+            <span>Influencers</span>
+            <span>Affiliates</span>
+            <span>Podcast</span>
+            <span>Invite a Friend</span>
+            <span>Become a Seller</span>
+            <span>Community Standards</span>
+          </div>
+          <div className='item'>
+            <h2>More From Fiverr</h2>
+            <span>Flybeer Business</span>
+            <span>Flybeer Pro</span>
+            <span>Flybeer Logo Maker</span>
+            <span>Flybeer Guides</span>
+            <span>Get Inspired</span>
+            <span>Flybeer Select</span>
+            <span>ClearVoice</span>
+            <span>Flybeer Workspace</span>
+            <span>Learn</span>
+            <span>Working Not Working</span>
+          </div>
+        </div>
+        <hr />
+        <div className='bottom'>
+          <div className='left'>
+            <h2>Flybeer</h2>
+            <span>© Flybeer International Ltd. 2023</span>
+          </div>
+          <div className='right'>
+            <div className='social'>
+              <img src='/img/twitter.png' alt='' />
+              <img src='/img/facebook.png' alt='' />
+              <img src='/img/linkedin.png' alt='' />
+              <img src='/img/pinterest.png' alt='' />
+              <img src='/img/instagram.png' alt='' />
+            </div>
+            <div className='link'>
+              <img src='/img/language.png' alt='' />
+              <span>English</span>
+            </div>
+            <div className='link'>
+              <img src='/img/coin.png' alt='' />
+              <span>USD</span>
+            </div>
+            <img src='/img/accessibility.png' alt='' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Footer;
